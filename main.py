8dfb79c99deef9d7ea3428b4d7a42eae535b7aad
Leo Qin <leozqin@gmail.com>
2025-01-04 14:18:03 -0800

    feat: support streaming and nonstreaming response

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from ollama import AsyncClient
+from ollama import Client
 from ollama._types import ChatRequest, ChatResponse, GenerateRequest, GenerateResponse
 from pydantic import BaseModel, ConfigDict
 from random import choice
-from typing import List, Mapping
+from typing import Set, Mapping, Union, Iterator, Generator
+from types import GeneratorType
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse, StreamingResponse, JSONResponse
 from logging import getLogger
 from contextlib import asynccontextmanager
 
@@ -12,11 +14,11 @@ logger = getLogger("uvicorn.error")
 app = FastAPI()
 
 
-class ProxyClient(BaseModel):
+class ProxyClient(BaseModel, frozen=True):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     host: str
-    ollama_client: AsyncClient
+    ollama_client: Client
 
 
 class EvalResponse(BaseModel):
@@ -29,29 +31,31 @@ class ModelNotFoundError(Exception):
     pass
 
 
-hosts = ["localhost:8001", "localhost:8002", "localhost:8003", "localhost:8004"]
+hosts = ["ollama0:11434"]
 
 clients = {
-    host: ProxyClient(host=host, ollama_client=AsyncClient(host=host)) for host in hosts
+    host: ProxyClient(host=host, ollama_client=Client(host=host)) for host in hosts
 }
 
-supported: Mapping[str, List[AsyncClient]] = {}
+supported: Mapping[str, Set[Client]] = {}
+
 
 async def refresh_model_registry():
     for client in clients.values():
-        models = await client.ollama_client.list()
+        models = client.ollama_client.list()
         for model in models.models:
             if model.model not in supported:
-                supported[model.model] = [client]
+                supported[model.model] = set([client])
             else:
-                supported[model.model].append(client)
+                supported[model.model].add(client)
+
 
 async def pick_random_supported_client(model: str) -> ProxyClient:
     if model in supported:
         logger.info(
             f"model {model} supported by hosts {[i.host for i in supported[model]]}"
         )
-        return choice(supported[model])
+        return choice(list(supported[model]))
     else:
         await refresh_model_registry()
         if model not in supported:
@@ -62,29 +66,65 @@ async def pick_random_supported_client(model: str) -> ProxyClient:
         logger.info(
             f"model {model} supported by hosts {[i.host for i in supported[model]]}"
         )
-        return choice(supported[model])
+        return choice(list(supported[model]))
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await refresh_model_registry
-    
+    await refresh_model_registry()
+
     yield
 
 
 @app.post("/api/chat")
-async def chat(req: ChatRequest) -> ChatResponse:
+async def chat(req: ChatRequest):
     model = req.model
+    stream = req.stream
     client = await pick_random_supported_client(model=model)
 
     logger.info(f"Chose host {client.host} to serve request")
-    return await client.ollama_client.chat(**req.model_dump())
+    resp: Union[ChatResponse, Iterator[ChatResponse]] = client.ollama_client.chat(**req.model_dump())
+
+    async def streamer(iter: Iterator[ChatResponse]):
+        for i in iter:
+            yield i.model_dump_json()
+    
+    if stream:
+        return StreamingResponse(content=streamer(resp))
+    else:
+        return JSONResponse(content=resp.model_dump_json())
 
 
 @app.post("/api/generate")
-async def chat(req: GenerateRequest) -> GenerateResponse:
+async def generate(
+    req: GenerateRequest,
+):
     model = req.model
+    stream = req.stream
     client = await pick_random_supported_client(model=model)
 
     logger.info(f"Chose host {client.host} to serve request")
-    return await client.ollama_client.generate(**req.model_dump())
+    resp: Union[GenerateResponse, Iterator[GenerateResponse]] = (
+        client.ollama_client.generate(**req.model_dump())
+    )
 
+    async def streamer(iter: Iterator[GenerateResponse]):
+        for i in iter:
+            yield i.model_dump_json()
+        
+
+    if stream:
+        return StreamingResponse(content=streamer(resp))
+    else:
+        return JSONResponse(content=resp.model_dump_json())
+
+
+@app.get("/models")
+async def models():
+    await refresh_model_registry()
+    return {model: [i.host for i in clients] for model, clients in supported.items()}
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def root() -> str:
+    return "Ollama is running"
